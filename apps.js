edc6dd6cd33fb68ada5ec0c705b9fd3136340618
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-10-04 19:32:29 +0100

    work complete

---
+++ apps.js
@@ -0,0 +1,93 @@
+
+
+// function sum(a,b){
+// console.log(a,b)
+// }
+// sum(5.5*30)
+
+
+// question 1//
+
+// function sum(){
+// console.log(new Date)
+// }
+// sum( new Date())
+
+
+
+// question 2//
+// function sum(num1,num2){
+//     console.log(num1 + num2)
+// }
+
+// var num1 = +prompt("enter your number")
+// var num2 = +prompt("Enter your second  number")
+
+// sum(num1,num2)
+
+
+
+// question 3//
+
+// function sum(num1,opr,num2){
+// if(opr=== "+"){
+//     return num1 + num2;
+
+// }else if (opr==="*"){
+
+//     return num1 * num2;
+
+// }else if (opr==="/"){
+
+//     return num1 / num2;
+
+// }else if ( opr==="-"){
+
+//     return num1 - num2;
+// }
+// }
+// var num2 = +prompt("Enter your second value")
+// var opr = prompt("Enter your operator value")
+// var num1 = +prompt("Enter your first value")
+// var result = sum(num1,opr,num2)
+// console.log(result)
+
+
+
+// queastion 4//
+
+
+// function sum(na){
+//     console.log(na)
+// }
+// sum("Hello sir ")
+
+
+
+// question 5 //
+
+// function cont(start,end){
+//     if(start<=end){
+//         for(var i =start; i <=end; i++){
+
+//         }
+//     }
+// }
+
+// var start = +prompt("Enteer your number")
+// // var end = +prompt("Enter your second number")
+
+// var result = cont(start)
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
